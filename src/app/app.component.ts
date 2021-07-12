fcf61e33be4a284eb3b8115d6cca448cdcbd919f
peesaeung <peeranut_saeung@hotmail.com>
2021-07-12 14:57:07 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {translateFiles} from '@angular/localize/src/tools/src/translate/main';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's2q1';
+  num = new FormControl('');
+  result: boolean;
+  fList: any = ['isPrime', 'isFibonacci'];
+  fChoice = new FormControl('', Validators.required);
+  isPrime(n){
+    if (n <= 1)
+      return false;
+    // Check from 2 to n-1
+    for (let i = 2; i < n; i++)
+      if (n % i === 0)
+        return false;
+    return true;
+  }
+  isPerfectSquare(x) {
+    const s = Math.sqrt(x);
+    return (s * s === x);
+  }
+// Returns true if n is a Fibonacci Number, else false
+  isFibonacci(n) {
+      // n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both
+      // is a perfect square
+    return (this.isPerfectSquare((5 * (n ** 2) + 4) || this.isPerfectSquare((5 * (n ** 2)) - 4)));
+  }
+
+  selected(e) {
+    console.log(e.target.value);
+    if (this.fChoice.value === 'isPrime'){
+      this.result = this.isPrime(this.num.value);
+    }
+    else if (this.fChoice.value === 'isFibonacci') {
+      this.result = this.isFibonacci(this.num.value);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
